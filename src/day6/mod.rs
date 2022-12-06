0c7521a508befbef611bd2453cad7735258308c9
Stefan Hartnagel <stefan.hartnagel@ul.com>
2022-12-06 10:37:08 +0100

    refactor

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -1,20 +1,15 @@
 use std::{collections::HashSet, fs};
 
-fn find_distinct_characters(input: &String, num_of_distinct_chars: usize) -> i32 {
-    let mut index = 0;
-    let mut current_sequence: Vec<char> = Vec::new();
-    for char in input.chars() {
-        current_sequence.push(char);
-        index += 1;
-        if current_sequence.len() == num_of_distinct_chars {
-            let sequence_hash: HashSet<char> = HashSet::from_iter(current_sequence.clone());
-            if sequence_hash.len() == num_of_distinct_chars {
-                return index;
-            }
-            current_sequence.remove(0);
+fn find_distinct_characters(input: &String, num_of_distinct_chars: usize) -> usize {
+    let char_vec_input:Vec<char> = input.chars().collect();
+    for index in num_of_distinct_chars..input.chars().count() {
+        let current_slice = char_vec_input[index-num_of_distinct_chars..index].to_vec();
+        let sequence_hash: HashSet<char> = HashSet::from_iter(current_slice.to_vec());
+        if sequence_hash.len() == num_of_distinct_chars{
+            return index;
         }
     }
-    return index;
+    return 0;
 }
 pub fn solver() {
     let input =
