30e675126bef6dc0982c609561a17dbfb60a2d71
Stefan Hartnagel <stefan.hartnagel@ul.com>
2022-12-07 10:52:51 +0100

    refactor day7

--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -1,15 +1,16 @@
 use std::{collections::HashMap, fs};
 
-fn convert_to_full_name(current_dictionary: &Vec<&str>) -> String {
-    let mut directory: String = String::from("");
-    if current_dictionary.len() == 1 {
-        return "/".to_owned();
+fn convert_to_full_name(current_directory: &Vec<&str>) -> String {
+    if current_directory.len() == 1 {
+        return current_directory[0].to_owned();
     }
-    for current_path in &current_dictionary[1..] {
-        directory += "/";
-        directory += &current_path.to_string();
+
+    let mut full_directory_name: String = String::from("");
+    for current_path in &current_directory[1..] {
+        full_directory_name += "/";
+        full_directory_name += &current_path.to_string();
     }
-    return directory;
+    return full_directory_name;
 }
 
 fn add_size_to_path(
@@ -28,15 +29,10 @@ fn add_size_to_path(
     } else {
         directories.insert(directory_name, size);
     }
-    let mut current_directory_recursive = current_directory.clone();
-    current_directory_recursive.pop();
-    add_size_to_path(directories, &current_directory_recursive, size);
+    add_size_to_path(directories, &current_directory[..current_directory.len() - 1 ].to_vec(), size);
 }
 
-pub fn solver() {
-    let input =
-        fs::read_to_string("./src/day7/input.txt").expect("Should have been able to read the file");
-    let commands: Vec<&str> = input.split("\r\n").collect();
+fn build_directories(commands: &Vec<&str>) -> HashMap<String, usize> {
     let mut directories: HashMap<String, usize> = HashMap::new();
     let mut current_directory: Vec<&str> = Vec::new();
     for command in commands {
@@ -57,34 +53,50 @@ pub fn solver() {
         let size: usize = size_str[0].parse().unwrap();
         add_size_to_path(&mut directories, &current_directory, size);
     }
+    return directories;
+}
 
-    let mut sum_part_1 = 0;
-    for (key, value) in &directories {
+fn calculate_part_one(directories: &HashMap<String, usize>) -> usize {
+    let mut sum = 0;
+    for (_, value) in directories {
         if value < &(100000 as usize) {
-            sum_part_1 += value;
+            sum += value;
         }
-        println!("{}: {}", key, value);
     }
-    
-    println!("Day7:");
-    println!("First part: {sum_part_1}");
+    return sum;
+}
 
+fn calculate_part_two(directories: &HashMap<String, usize>) -> usize {
     let total_disk_space = 70000000 as usize;
     let total_disk_usage = directories["/"];
     let total_disk_usage_goal = 30000000 as usize;
     let free_space = total_disk_space - total_disk_usage;
     if free_space > total_disk_usage_goal {
         println!("Already enough space");
-        return;
+        return 0;
     }
 
     let size_to_be_freed = total_disk_usage_goal - free_space;
     println!("Size to be freed: {}", size_to_be_freed);
     let mut optimal_size = total_disk_space;
-    for (_, value) in &directories {
+    for (_, value) in directories {
         if value > &size_to_be_freed && value < &optimal_size {
             optimal_size = *value;
         }
     }
-    println!("Second part: {optimal_size}");
+    return optimal_size;
+}
+
+pub fn solver() {
+    let input =
+        fs::read_to_string("./src/day7/input.txt").expect("Should have been able to read the file");
+    let commands: Vec<&str> = input.split("\r\n").collect();
+    let directories = build_directories(&commands);
+
+    let sum_part_one = calculate_part_one(&directories);
+    let part_two = calculate_part_two(&directories);
+
+    println!("Day7:");
+    println!("First part: {sum_part_one}");
+    println!("Second part: {part_two}");
 }
