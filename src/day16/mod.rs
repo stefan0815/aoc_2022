170cdec3f50e68ab00bcb5aacad48498843fa894
Stefan Hartnagel <hartnagel.stefan@web.de>
2022-12-17 01:12:06 +0100

    day 16 iteratively improving recursive search

--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -8,24 +8,25 @@ struct Valve {
     tunnels: Vec<String>,
 }
 
-fn open_valve(valves: &mut HashMap<String, Valve>, valve: &String, time: usize) -> usize {
-    let value = valves[valve].value(0, time);
+fn open_valve(valves: &mut HashMap<String, Valve>, valve: &String, time: &mut usize) -> usize {
+    *time -= 1;
+    let value = valves[valve].flow_rate * *time;
     valves.get_mut(valve).unwrap().activated = true;
     return value;
 }
 
-fn path_from_to(came_from: &HashMap<String, String>, from: &String, to: &String) -> Vec<String>{
-    let mut current_node = to.to_string();
-    let mut path: Vec<String> = Vec::new();
-    loop {
-        path.push(current_node.to_string());
-        if current_node == *from {
-            path.reverse();
-            return path;
-        }
-        current_node = came_from[&current_node].to_string();
-    }
-}
+// fn path_from_to(came_from: &HashMap<String, String>, from: &String, to: &String) -> Vec<String> {
+//     let mut current_node = to.to_string();
+//     let mut path: Vec<String> = Vec::new();
+//     loop {
+//         path.push(current_node.to_string());
+//         if current_node == *from {
+//             path.reverse();
+//             return path;
+//         }
+//         current_node = came_from[&current_node].to_string();
+//     }
+// }
 
 fn a_star_search(
     valves: &HashMap<String, Valve>,
@@ -78,21 +79,101 @@ impl Valve {
     // }
 }
 
-fn find_best_valve(valves: &HashMap<String,Valve>, costs: &HashMap<String, usize>, time: usize) -> String{
-    let mut best_value = 0;
-    let mut best_valve: String = "".to_string();
-    for cost in costs {
-        if *cost.1 > time {
+// fn find_best_valve(
+//     valves: &HashMap<String, Valve>,
+//     distances: &HashMap<String, usize>,
+//     time: usize,
+// ) -> String {
+//     let mut best_value = 0;
+//     let mut best_valve: String = "".to_string();
+//     for cost in distances {
+//         if *cost.1 > time {
+//             continue;
+//         }
+//         let value = valves[cost.0].value(*cost.1, time);
+//         if value > best_value {
+//             best_value = value;
+//             best_valve = valves[cost.0].name.to_string();
+//             // println!("From {current} to {} distance is {} min, value: {}", cost.0, cost.1, value);
+//         }
+//     }
+//     return best_valve;
+// }
+
+fn get_remaining_valves_sorted(
+    valves: &HashMap<String, Valve>,
+    distances: &HashMap<String, usize>,
+    time: usize,
+    limit: usize,
+) -> Vec<(String, usize)> {
+    let mut valve_values: Vec<(String, usize)> = Vec::new();
+    for (valve_name, valve) in valves {
+        let distance = distances[valve_name];
+        if valve.activated || valve.flow_rate == 0 || distance as i32 >= time as i32 {
             continue;
         }
-        let value = valves[cost.0].value(*cost.1, time);
-        if value > best_value {
-            best_value = value;
-            best_valve = valves[cost.0].name.to_string();
-            // println!("From {current} to {} distance is {} min, value: {}", cost.0, cost.1, value);
-        }           
+        valve_values.push((valve_name.to_string(), valve.value(distance, time)));
+        //distance
     }
-    return best_valve
+    valve_values.sort_by(|a, b| a.1.cmp(&b.1));
+    valve_values.reverse();
+    if limit != 0 && valve_values.len() > limit {
+        return valve_values[..limit].to_vec();
+    }
+    return valve_values;
+}
+
+fn get_available_pressure_release(valves: &HashMap<String, Valve>, time: usize) -> i32 {
+    let mut accumulated_value = 0;
+    for (_, valve) in valves {
+        accumulated_value += valve.value(0, time);
+    }
+    return accumulated_value as i32;
+}
+
+fn solve_part_one_step(
+    valves: &mut HashMap<String, Valve>,
+    current: &String,
+    time: usize,
+    early_break_limit: i32,
+    limit: usize,
+    depth: usize,
+) -> (usize, Vec<String>) {
+    let releasable_pressure = get_available_pressure_release(&valves, time);
+    // println!("{releasable_pressure}/{early_break_limit}");
+    if releasable_pressure < early_break_limit {
+        return (0, [].to_vec());
+    }
+    // println!("{}, {}", time, depth);
+    let (distances, _) = a_star_search(&valves, &current);
+    // for (new_node, dist) in &distances{
+    //     println!("Distance from: {current} to {new_node} is {dist}min");
+    // }
+    let remaining_valves = get_remaining_valves_sorted(&valves, &distances, time, limit);
+    let mut pressure_release_max = 0;
+    let mut opened_valves_max: Vec<String> = Vec::new();
+    for (new_valve, _) in &remaining_valves {
+        let mut new_valves = valves.clone();
+        let mut new_time = time - distances[&new_valve.to_string()];
+        let pressure_valve = open_valve(&mut new_valves, new_valve, &mut new_time);
+        // println!("Distance: {}, Open valve: {new_valve}, releasing pressure: {pressure}, new_time: {new_time}",  distances[&new_valve.to_string()]);
+        let (pressure, mut opened_valves) = solve_part_one_step(
+            &mut new_valves.clone(),
+            &new_valve,
+            new_time,
+            early_break_limit - pressure_valve as i32,
+            limit,
+            depth + 1,
+        );
+        if pressure + pressure_valve > pressure_release_max {
+            pressure_release_max = pressure + pressure_valve;
+            opened_valves_max = Vec::new();
+            opened_valves_max.push(new_valve.to_string());
+            opened_valves_max.append(&mut opened_valves);
+        }
+    }
+
+    return (pressure_release_max, opened_valves_max);
 }
 
 pub fn solver() {
@@ -117,9 +198,9 @@ pub fn solver() {
         let tunnels_str: Vec<&str> = split_two[1].split(", ").collect();
         let mut tunnels: Vec<String> = Vec::new();
 
-        println!("Name: {}, Flowrate: {}, Tunnels:", name, flow_rate);
+        // println!("Name: {}, Flowrate: {}, Tunnels:", name, flow_rate);
         for tunnel in &tunnels_str {
-            print!("{tunnel}, ");
+            // print!("{tunnel}, ");
             tunnels.push(tunnel.to_string());
         }
         println!();
@@ -134,28 +215,28 @@ pub fn solver() {
         );
     }
 
-    let mut current = start.to_string();
-    let mut time = 30;
-    let mut total_pressure_released = 0;
-    while time > 0 {
-        let value_of_current = valves[&current].value(0, time - 1);
-        println!("{value_of_current}");
-        if value_of_current > 0 {
-            total_pressure_released += open_valve(&mut valves, &current, time);
-            time -= 1;
-            continue;
+    let time = 30;
+    let mut early_break_limit = 0;
+    let mut max_total_pressure_released = 0;
+    for limit in [1, 3, 5, 7, 9, 0] {
+        let (total_pressure_released, _) = solve_part_one_step(
+            &mut valves,
+            &start.to_string(),
+            time,
+            early_break_limit,
+            limit,
+            0,
+        );
+        if total_pressure_released > max_total_pressure_released {
+            max_total_pressure_released = total_pressure_released;
+            early_break_limit = total_pressure_released as i32;
         }
-        let (costs, came_from) = a_star_search(&valves, &current);       
-        let next_valve = find_best_valve(&valves, &costs, time - 1);
-        if next_valve == ""{
-            break;
-        }
-
-        let path = path_from_to(&came_from, &current, &next_valve);
-        current = path[1..].first().unwrap().to_string();
-        time -= 1;
+        println!("total_pressure_released: {total_pressure_released} with early_break_limit: {early_break_limit} and limit: {limit}");
+        // for valve in &opened_valves {
+        //     println!("Opened: {valve}");
+        // }
     }
 
     println!("Day16:");
-    println!("Greedy valve search: {total_pressure_released}");
+    println!("Recursive valve search: {max_total_pressure_released}");
 }
